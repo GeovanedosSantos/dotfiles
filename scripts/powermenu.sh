8402f4455e82ac17775dbed897d00bc8c62d95f9
GeovanedosSantos <geovanemartins45776@gmail.com>
2025-08-07 15:11:17 -0300

    Ultimas atualizações

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-lock="swaylock -f -e"
 function powermenu {
-    option="Desligar\nReiniciar\nSuspender\nBloquear"
-    selected=$(echo -e $option | fuzzel --dmenu -l 4) || exit 0
+    option="Desligar\nReiniciar\nSuspender"
+    selected=$(echo -e $option | fuzzel --dmenu -l 3) || exit 0
     if [[ $selected = "Desligar" ]]; then
         poweroff
     elif [[ $selected = "Reiniciar" ]]; then
         reboot
     elif [[ $selected = "Suspender" ]]; then
         systemctl suspend
-    elif [[ $selected = "Bloquear" ]]; then
-        $lock
     fi
 }
 
