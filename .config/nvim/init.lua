961ed8d1894ccc37370a52d6f2e7552cc62d1c44
GeovanedosSantos <geovanemartins45776@gmail.com>
2025-08-09 20:13:35 -0300

    Consertando problemas no niri e adicionando o neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,15 @@
+require("config.lazy")
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 10
+vim.opt.history = 1000
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keymaps 
+vim.keymap.set("n","<leader>e", ":Ex<CR>", { noremap = true })
+
+vim.keymap.set("x", "<leader>c" , ':! tr -s " " | column -t -s "|" -o "|"<CR>', { noremap = true })
